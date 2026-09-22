9b34ad0640d1bf3068062de882ff6c9d79c9c07c
HM Zihan <zihanz696@gmail.com>
2026-09-22 13:17:37 +0600

    Deleting_Index
    Deleting the index of a word by using C# language

---
+++ DeleteingIndex/DeleteingIndex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Xi
+{
+
+    public static string remove_char(string str, int n)
+    {
+        return str.Remove(n, 1);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(remove_char("w3resource", 3));
+        Console.WriteLine(remove_char("w3resource", 2));
+        Console.WriteLine(remove_char("w3resource", 4));
+    }
+}
